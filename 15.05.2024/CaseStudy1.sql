65e3099b9e9b48d194857302f8f09fb5026a9e3a
Asgard09 <thanhphuc04nh@gmail.com>
2025-05-19 14:48:29 +0700

    qs 6, 7

--- 15.05.2024/CaseStudy1.sql
+++ 15.05.2024/CaseStudy1.sql
@@ -18,7 +18,7 @@ group by customer_id;
 
 -- ROW_NUMBER() là một window function dùng để đánh số thứ tự từng dòng trong một nhóm (PARTITION) 
 -- theo thứ tự chỉ định.
-with first_purchased as(
+with first_purchased_in_menu as(
 	select s.customer_id, s.product_id, m.product_name, s.order_date,
 		ROW_NUMBER() over(
 			partition by (s.customer_id)
@@ -29,7 +29,7 @@ with first_purchased as(
 )
 
 select customer_id, product_name, order_date
-from first_purchased
+from first_purchased_in_menu
 where rank = 1;
 
 
@@ -55,5 +55,39 @@ with most_popular as (
 select customer_id, product_name, order_count
 from most_popular
 where rank = 1;
+
+-- 6. Which item was purchased first by the customer after they became a member?
+with first_purchased_when_became_member as(
+	select sales.customer_id, menu.product_name, sales.order_date, members.join_date,
+		dense_rank() over(
+			partition by sales.customer_id
+			order by sales.order_date
+		) as rank
+	from sales 
+	join menu on sales.product_id = menu.product_id
+	join members on sales.customer_id =  members.customer_id
+	and sales.order_date > members.join_date
+)
+
+select customer_id, product_name, order_date, join_date
+from first_purchased_when_became_member
+
+
+-- 7. Which item was purchased just before the customer became a member?
+with item_purchased_before_member as (
+	select sales.customer_id, menu.product_name, sales.order_date, members.join_date,
+		row_number() over(
+			partition by sales.customer_id
+			order by sales.order_date desc
+		) as row_num
+	from sales 
+	join menu on sales.product_id = menu.product_id
+	join members on sales.customer_id =  members.customer_id
+	and sales.order_date < members.join_date
+)
+
+select customer_id, product_name, order_date, join_date
+from item_purchased_before_member
+where row_num = 1;
 
 
