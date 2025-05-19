c5bc8e254d58d9bca637d584f4fb1e3eec5ed255
Asgard09 <thanhphuc04nh@gmail.com>
2025-05-19 14:01:25 +0700

    update for q3, q5

--- 15.05.2024/CaseStudy1.sql
+++ 15.05.2024/CaseStudy1.sql
@@ -18,17 +18,20 @@ group by customer_id;
 
 -- ROW_NUMBER() là một window function dùng để đánh số thứ tự từng dòng trong một nhóm (PARTITION) 
 -- theo thứ tự chỉ định.
-select customer_id, m.product_name, order_date
-from(
-	select s.customer_id, s.order_date, s.product_id,
-	row_number() over(
-		partition by s.customer_id
-		order by s.order_date
-	) as rn
-	from sales as s
-) as r
-join menu as m on r.product_id = m.product_id
-where rn = 1;
+with first_purchased as(
+	select s.customer_id, s.product_id, m.product_name, s.order_date,
+		ROW_NUMBER() over(
+			partition by (s.customer_id)
+			order by (s.order_date)
+		) as rank
+	from sales s
+	join menu m on s.product_id = m.product_id
+)
+
+select customer_id, product_name, order_date
+from first_purchased
+where rank = 1;
+
 
 -- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
 select top 1 m.product_name, COUNT(s.product_id) as most_purchased_item
@@ -38,23 +41,19 @@ group by m.product_name
 order by most_purchased_item DESC
 
 -- 5. Which item was the most popular for each customer?
-WITH most_popular AS (
-  SELECT sales.customer_id, menu.product_name, COUNT(menu.product_id) AS order_count,
-  dense_rank() OVER (
-	PARTITION BY sales.customer_id 
-    ORDER BY COUNT(sales.customer_id) DESC
-  ) AS rank
-  FROM [dbo].[menu]
-  INNER JOIN [dbo].[sales] ON menu.product_id = sales.product_id
-  GROUP BY sales.customer_id, menu.product_name
+with most_popular as (
+	select s.customer_id, m.product_name, COUNT(s.product_id) AS order_count,
+		dense_rank() over(
+			partition by s.customer_id 
+			order by count(s.product_id) desc
+		) as rank
+	from sales s
+	join menu m on s.product_id = m.product_id
+	group by s.customer_id, m.product_name
 )
 
-SELECT 
-  customer_id, 
-  product_name, 
-  order_count
-FROM most_popular 
-WHERE rank = 1;
-
+select customer_id, product_name, order_count
+from most_popular
+where rank = 1;
 
 
