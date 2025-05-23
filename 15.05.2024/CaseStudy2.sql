12437695cf422c752a2d27dbcdf5fcca98237abf
Asgard09 <thanhphuc04nh@gmail.com>
2025-05-23 17:42:29 +0700

    update

--- 15.05.2024/CaseStudy2.sql
+++ 15.05.2024/CaseStudy2.sql
@@ -13,3 +13,21 @@ select runner_id, COUNT(order_id) AS successful_orders
 from runner_orders
 where TRY_CAST(REPLACE(distance, 'km', '') AS float) is not null
 GROUP BY runner_id;
+
+-- 4. How many of each type of pizza was delivered?
+SELECT CAST(p.pizza_name as varchar) as pizza_name, COUNT(c.pizza_id) AS delivered_pizza_count
+FROM customer_orders AS c
+JOIN runner_orders AS r
+  ON c.order_id = r.order_id
+JOIN pizza_names AS p
+  ON c.pizza_id = p.pizza_id
+WHERE TRY_CAST(REPLACE(distance, 'km', '') AS float) is not null
+GROUP BY CAST(p.pizza_name as varchar);
+
+-- B. Runner and Customer Experience
+-- 4. What was the average distance travelled for each customer?
+select customer_id, AVG(TRY_CAST(REPLACE(distance, 'km', '') AS float)) as average_distance
+from runner_orders as r
+join customer_orders as c on r.order_id = c.order_id
+group by customer_id
+
