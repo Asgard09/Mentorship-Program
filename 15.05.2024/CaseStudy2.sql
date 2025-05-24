35e51e7febfa2db8a2e658303bb5b1df5dd327cd
Asgard09 <thanhphuc04nh@gmail.com>
2025-05-24 18:02:00 +0700

    qs5, 6

--- 15.05.2024/CaseStudy2.sql
+++ 15.05.2024/CaseStudy2.sql
@@ -24,6 +24,31 @@ JOIN pizza_names AS p
 WHERE TRY_CAST(REPLACE(distance, 'km', '') AS float) is not null
 GROUP BY CAST(p.pizza_name as varchar);
 
+-- 5. How many Vegetarian and Meatlovers were ordered by each customer?
+select c.customer_id, CAST(p.pizza_name as varchar) as pizza_name, count(CAST(p.pizza_name as varchar)) as order_count
+from customer_orders as c
+join pizza_names as p on c.pizza_id = p.pizza_id
+group by c.customer_id, CAST(p.pizza_name as varchar)
+
+-- 6. What was the maximum number of pizzas delivered in a single order?
+WITH pizza_count_cte AS
+(
+  SELECT 
+    c.order_id, 
+    COUNT(c.pizza_id) AS pizza_per_order
+  FROM customer_orders AS c
+  JOIN runner_orders AS r
+    ON c.order_id = r.order_id
+  WHERE TRY_CAST(REPLACE(distance, 'km', '') AS float) != 0
+  GROUP BY c.order_id
+)
+
+SELECT 
+  MAX(pizza_per_order) AS pizza_count
+FROM pizza_count_cte;
+
+
+-- 7. 
 -- B. Runner and Customer Experience
 -- 4. What was the average distance travelled for each customer?
 select customer_id, AVG(TRY_CAST(REPLACE(distance, 'km', '') AS float)) as average_distance
