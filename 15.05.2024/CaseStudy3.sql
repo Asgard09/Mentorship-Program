18bfc227620f15f842ce0a8b103bf13d694f5bb4
Asgard09 <thanhphuc04nh@gmail.com>
2025-06-05 15:29:23 +0700

    qs1, 2, 3 for cs 3

--- 15.05.2024/CaseStudy3.sql
+++ 15.05.2024/CaseStudy3.sql
@@ -6,3 +6,31 @@ FROM plans as p
 JOIN subscriptions as s
 on p.plan_id = s.plan_id
 
+-- B. Data Analysis Questions
+-- 1. How many customers has Foodie-Fi ever had?
+SELECT COUNT(DISTINCT s.customer_id) as count_customers
+FROM subscriptions as s
+
+-- 2. What is the monthly distribution of trial plan start_date values for our dataset 
+-- use the start of the month as the group by value
+SELECT DATEPART(month, s.start_date) AS month_date, DATENAME(month, s.start_date) as month_name,  COUNT(*) as count
+FROM subscriptions as s
+JOIN plans as p on s.plan_id = p.plan_id
+WHERE p.plan_name = 'trial'
+GROUP BY DATENAME(month, s.start_date), DATEPART(month, s.start_date)
+ORDER BY DATEPART(month, s.start_date)
+
+-- 3. What plan start_date values occur after the year 2020 for our dataset? 
+-- Show the breakdown by count of events for each plan_name.
+SELECT p.plan_id, p.plan_name, COUNT(s.customer_id) as num_of_events
+FROM subscriptions as s
+JOIN plans as p on s.plan_id = p.plan_id
+WHERE s.start_date > '2021-01-01'
+GROUP BY p.plan_id, p.plan_name
+ORDER BY p.plan_id
+
+-- 4. What is the customer count and percentage of customers who have churned rounded to 1 decimal place?
+SELECT *
+FROM subscriptions as s
+JOIN plans as p on s.plan_id = p.plan_id
+WHERE p.plan_name = 'churn'
